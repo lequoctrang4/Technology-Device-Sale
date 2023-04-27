6fa293a01505520039ec1f93386b7bbc2707e648
trinitrobenzene <trannhubuu1412@gmail.com>
2023-04-27 08:03:04 +0700

    nothing

--- model/eOrder.ts
+++ model/eOrder.ts
@@ -1,10 +1,30 @@
-import eProduct from "./eProduct";
+interface pOrder {
+    "id": string,
+    "name": string,
+    "color": string,
+    "salePercent": string,
+    "price": string,
+    "manufacturer": string,
+    "html": string,
+    "image": string,
+    "productID": string,
+    "orderID": string,
+    "discount": string,
+    "quantity": string
+}
 
 interface eOrder {
-    id: string,
-    status: string,
-    userDetail: any,
-    detail: Array<eProduct>
+    "orderId": string,
+    "userId": string,
+    "sessionId": string,
+    "token": string,
+    "status": string,
+    "tax": string,
+    "subTotal": string,
+    "voucherId": string,
+    "shippingId": string,
+    "note": string,
+    products: pOrder[]
 }
 
 export default eOrder;
